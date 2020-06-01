64e070d7d13c426f75031e1d5cc918f5566e2d7d
James Peach <jpeach@apache.org>
2020-06-01 13:42:00 +1000

    Revert enabling tracing by default.

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ var (
 	cqueryPath = pflag.StringP("cquery", "c", "clangd", "Path to the cquery binary")
 	debugLsp   = pflag.Bool("debug-lsp", false, "Enable cquery debug output")
 	helpFlag   = pflag.BoolP("help", "h", false, "Print this help message")
-	traceFile  = pflag.String("trace", "/tmp/cscope.trace", "Trace cscope messages to the given file")
+	traceFile  = pflag.String("trace", "", "Trace cscope messages to the given file")
 	traceLsp   = pflag.Bool("trace-lsp", true, "Trace LSP messages to the trace file")
 
 	// The following flags are required for cscope compatibility. Vim will
